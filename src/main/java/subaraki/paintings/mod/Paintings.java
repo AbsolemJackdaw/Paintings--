176b962d5850c8971a3e43dcc55e881319267275
ArtixAllMighty <robbeenaxel@hotmail.com>
2018-01-25 19:35:08 +0100

    versioning trough build

--- src/main/java/subaraki/paintings/mod/Paintings.java
+++ src/main/java/subaraki/paintings/mod/Paintings.java
@@ -29,7 +29,7 @@ import subaraki.paintings.mod.server.proxy.CommonProxy;
 public class Paintings {
 
     public static final String MODID = "morepaintings";
-    public static final String VERSION = "1.11.2-3.2.1.1";
+    public static final String VERSION = "$version";
     public static final String NAME = "Paintings++";
 
     @SidedProxy(serverSide = "subaraki.paintings.mod.server.proxy.CommonProxy", clientSide = "subaraki.paintings.mod.client.proxy.ClientProxy")
