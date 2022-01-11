ece268d4b088b2cb9626430e2afb9645558045a9
Subaraki <robbeenaxel@hotmail.com>
2022-01-12 00:34:20 +0100

    append fabric config values to common

--- Fabric/src/main/java/subaraki/paintings/util/ModConfig.java
+++ Fabric/src/main/java/subaraki/paintings/util/ModConfig.java
@@ -5,8 +5,18 @@ import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name = "paintings")
 public class ModConfig implements ConfigData {
+
     public boolean use_vanilla_only = false;
     public boolean use_selection_gui = true;
     public boolean cycle_paintings = false;
     public boolean show_painting_size = true;
+
+    @Override
+    public void validatePostLoad() throws ValidationException {
+        ConfigData.super.validatePostLoad();
+        CommonConfig.use_vanilla_only = use_vanilla_only;
+        CommonConfig.use_selection_gui = use_selection_gui;
+        CommonConfig.cycle_paintings = cycle_paintings;
+        CommonConfig.show_painting_size = show_painting_size;
+    }
 }
