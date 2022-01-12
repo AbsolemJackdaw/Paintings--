e9f6c5740ed944a519d73d1bc60a1d369bd8b50f
Subaraki <robbeenaxel@hotmail.com>
2022-01-12 18:42:40 +0100

    Fix fabric not being able to connect to forge servers

--- Forge/src/main/java/subaraki/paintings/network/NetworkHandler.java
+++ Forge/src/main/java/subaraki/paintings/network/NetworkHandler.java
@@ -7,10 +7,14 @@ import subaraki.paintings.network.server.SPacketPainting;
 
 public class NetworkHandler {
 
-    private static final String PROTOCOL_VERSION = "1";
+    private static final String PROTOCOL_VERSION = "1.1.0";
 
-    public static final SimpleChannel NETWORK = NetworkRegistry.newSimpleChannel(PacketId.CHANNEL,
-            () -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
+    public static final SimpleChannel NETWORK = NetworkRegistry.ChannelBuilder
+            .named(PacketId.CHANNEL)
+            .clientAcceptedVersions(NetworkRegistry.acceptMissingOr(PROTOCOL_VERSION::equals))
+            .serverAcceptedVersions(NetworkRegistry.acceptMissingOr(PROTOCOL_VERSION::equals))
+            .networkProtocolVersion(() -> PROTOCOL_VERSION)
+            .simpleChannel();
 
     public NetworkHandler() {
         new CPacketPainting().encrypt(PacketId.CPACKET);
