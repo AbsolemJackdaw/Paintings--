486c8d964063ad1fb41c0a141e860fee9b21e797
Subaraki <robbeenaxel@hotmail.com>
2022-06-15 03:22:17 +0200

    add gen for placeable tag

---
+++ Forge/src/main/java/subaraki/paintings/datagen/Generator.java
@@ -0,0 +1,30 @@
+package subaraki.paintings.datagen;
+
+import net.minecraft.core.Registry;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.tags.PaintingVariantTagsProvider;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.PaintingVariantTags;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+import subaraki.paintings.Paintings;
+import subaraki.paintings.utils.PaintingEntry;
+import subaraki.paintings.utils.PaintingPackReader;
+
+@Mod.EventBusSubscriber(modid = Paintings.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Generator {
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+
+        generator.addProvider(event.includeServer(), new PaintingVariantTagsProvider(generator, Paintings.MODID, event.getExistingFileHelper()) {
+            @Override
+            protected void addTags() {
+                for (PaintingEntry entry : PaintingPackReader.PAINTINGS)
+                    this.tag(PaintingVariantTags.PLACEABLE).add(ResourceKey.create(Registry.PAINTING_VARIANT_REGISTRY, entry.getResLoc()));
+            }
+        });
+    }
+}
