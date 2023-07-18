20b104c14cd7ff42f3e21c97d0fb16b4c506fab1
Dion Tryban <github@diontryban.com>
2023-07-18 04:34:50 -0400

    Finish port to 1.19.3 (#88)

--- Forge/src/main/java/subaraki/paintings/datagen/Generator.java
+++ Forge/src/main/java/subaraki/paintings/datagen/Generator.java
@@ -1,6 +1,7 @@
 package subaraki.paintings.datagen;
 
-import net.minecraft.core.Registry;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.PaintingVariantTagsProvider;
 import net.minecraft.resources.ResourceKey;
@@ -15,15 +16,21 @@ import subaraki.paintings.utils.PaintingPackReader;
 @Mod.EventBusSubscriber(modid = Paintings.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Generator {
 
+    // Apologise if this doesn't work. I've never used Forge's data generators before, but it compiles.
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
 
-        generator.addProvider(event.includeServer(), new PaintingVariantTagsProvider(generator, Paintings.MODID, event.getExistingFileHelper()) {
+        generator.addProvider(event.includeServer(), new PaintingVariantTagsProvider(
+                event.getGenerator().getPackOutput(),
+                event.getLookupProvider(),
+                Paintings.MODID,
+                event.getExistingFileHelper()
+        ) {
             @Override
-            protected void addTags() {
+            protected void addTags(HolderLookup.Provider provider) {
                 for (PaintingEntry entry : PaintingPackReader.PAINTINGS)
-                    this.tag(PaintingVariantTags.PLACEABLE).add(ResourceKey.create(Registry.PAINTING_VARIANT_REGISTRY, entry.getResLoc()));
+                    this.tag(PaintingVariantTags.PLACEABLE).add(ResourceKey.create(Registries.PAINTING_VARIANT, entry.getResLoc()));
             }
         });
     }
