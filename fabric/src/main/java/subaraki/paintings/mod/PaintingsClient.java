34d69a890b4e348fb5c8a3074654cb9a96e9224c
Absolem Jackdaw <absolemjackdaw@gmail.com>
2024-03-31 13:43:49 +0200

    fix network not registering packets on all sides

--- fabric/src/main/java/subaraki/paintings/mod/PaintingsClient.java
+++ fabric/src/main/java/subaraki/paintings/mod/PaintingsClient.java
@@ -1,12 +1,13 @@
 package subaraki.paintings.mod;
 
 import subaraki.paintings.events.ClientEvents;
+import subaraki.paintings.network.NetworkHandler;
 
 
 public class PaintingsClient implements net.fabricmc.api.ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        //NetworkHandler.registerClientPackets();
+        NetworkHandler.registerClientPackets();
         ClientEvents.fixBoundingBoxEvent();
     }
 }
