e0f7d191521a1a9b569df7a1450d55bf15c9812d
Absolem Jackdaw <absolemjackdaw@gmail.com>
2024-03-31 14:24:31 +0200

    register both packets on both loader sides

--- fabric/src/main/java/subaraki/paintings/mod/PaintingsClient.java
+++ fabric/src/main/java/subaraki/paintings/mod/PaintingsClient.java
@@ -7,6 +7,7 @@ import subaraki.paintings.network.NetworkHandler;
 public class PaintingsClient implements net.fabricmc.api.ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        NetworkHandler.registerServerPackets();
         NetworkHandler.registerClientPackets();
         ClientEvents.fixBoundingBoxEvent();
     }
